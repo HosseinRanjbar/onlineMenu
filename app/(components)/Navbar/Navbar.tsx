a9be28cefe59fc39fed4fc1c78eaad50912b718a
Sahab Ranjbar <amirisahab@gmail.com>
2024-10-19 02:01:22 +0330

    change Navbar className to justify-around

--- app/(components)/Navbar/Navbar.tsx
+++ app/(components)/Navbar/Navbar.tsx
@@ -17,36 +17,40 @@ const Navbar = () => {
     { id: 6, englishTitle: "cold-drink", farsiTitle: "نوشیدنی سرد" },
   ];
 
-
-
   return (
-
-      <div
-        className="flex justify-between items-center overflow-scroll w-screen gap-8 mt-5 p-2 overflow-y-hidden "
-
-        style={{scrollbarColor: "#D17842 black",scrollbarWidth: "thin",cursor: "pointer"}}
-      >
-        {navbarItem.map((item, index) => {
-          return (
-            <div className="flex flex-col items-center self-start text-sm text-nowrap" key={index}>
-              <Link href={`/${item.englishTitle}`}
-                className={
-                  param.items === item.englishTitle
-                    ? "text-[#D17842]"
-                    : "text-[#52555A]"
-                }
-                key={index}
-                data-value={item.englishTitle}
-              >
-                {item.farsiTitle}
-              </Link>
-              {param.items === item.englishTitle && (
-                <div className="rounded-full w-2 h-2 bg-[#D17842] m-2"></div>
-              )}
-            </div>
-          );
-        })}
-      </div>
+    <div
+      className="flex justify-around items-center overflow-scroll w-screen gap-8 mt-5 p-2 overflow-y-hidden"
+      style={{
+        scrollbarColor: "#D17842 black",
+        scrollbarWidth: "thin",
+        cursor: "pointer",
+      }}
+    >
+      {navbarItem.map((item, index) => {
+        return (
+          <div
+            className="flex flex-col items-center self-start text-sm text-nowrap"
+            key={index}
+          >
+            <Link
+              href={`/${item.englishTitle}`}
+              className={
+                param.items === item.englishTitle
+                  ? "text-[#D17842]"
+                  : "text-[#52555A]"
+              }
+              key={index}
+              data-value={item.englishTitle}
+            >
+              {item.farsiTitle}
+            </Link>
+            {param.items === item.englishTitle && (
+              <div className="rounded-full w-2 h-2 bg-[#D17842] m-2"></div>
+            )}
+          </div>
+        );
+      })}
+    </div>
 
   );
 };
